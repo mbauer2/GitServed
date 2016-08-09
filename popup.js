8adfce4924bff14a91eb89d984242708bf681a0f
Max <max.bauer7590@gmail.com>
2016-08-09 00:29:29 -0600

    Added jquery and rest call

--- popup.js
+++ popup.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+  var sendMessage = document.getElementById('sendMessage');
+  sendMessage.addEventListener('click', function() {
+	$.ajax({
+		type: "GET",
+		url: "https://api.github.com/repos/mbauer2/rayTracer/contents/README.md",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function (data, statuis, jqXHR) {
+			var sendMessage = document.getElementById('sendMessage');
+			var cont = atob(data["content"])
+			sendMessage.innerHTML = cont;
+		}
+	});
+	
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
   }, false);
 }, false);
+
