326868dcdcfebafdd89ff7ae61eda2c6aa6ab587
Michael Dunton <michael.dunton@tresta.com>
2016-08-22 15:45:52 -0400

    Using github personalized tokens, refactored duplicate code, and refactored to use ajax call backs a littler more smoothly.

--- popup.js
+++ popup.js
@@ -14,61 +14,56 @@ document.addEventListener('DOMContentLoaded', function() {
   
   var saveInfo = document.getElementById('saveInfo');
   saveInfo.addEventListener('click', function() {
-    var user = document.getElementById('username').value;
-	var pass = document.getElementById('pass').value;
+    var username = document.getElementById('username').value;
+	var token = document.getElementById('token').value;
 	var infoform = document.getElementById('userInfo');
 	infoform.reset();
-	window.name = JSON.stringify({"username": user, "password": pass, "sha": ""})
+	window.name = JSON.stringify({"username": username, "token": token, "sha": ""})
   }, false);
   
 }, false);
 
 function addMessage(message){
-    var owner = document.getElementById('owner').value;
-	var repo = document.getElementById('repo').value;
-    $.ajax({
-		type: "GET",
-		url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/README.md",
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		success: function (return_data, status, jqXHR) {
-			var cont = atob(return_data["content"]);
-			cont += "\n" + JSON.parse(window.name)["username"] + ": " + message;
-			var newCont = btoa(cont);
-			var newData = {"message":"GitServed commit", "content": newCont, "sha":return_data["sha"]}
-			var user = JSON.parse(window.name)["username"];
-			var pass = JSON.parse(window.name)["password"];
+    getFile(function (return_data, status, jqXHR) {
+            var owner = document.getElementById('owner').value;
+            var repo = document.getElementById('repo').value;
+            var cookieData = JSON.parse(window.name); //parse once 
+            var username = cookieData["username"];
+            var token = cookieData["token"];
+			var fileContent = atob(return_data["content"]);
+			var updatedContent = fileContent.concat("\n" + username + ": " + message);
+			var newData = {"message":"GitServed commit", "content": btoa(updatedContent), "sha":return_data["sha"]}
 			$.ajax({
 		        type: "PUT",
 		        url: "https://api.github.com/repos/" + owner +"/" + repo + "/contents/README.md",
 				beforeSend: function(xhr){
-				    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
+				    xhr.setRequestHeader("Authorization", "Token " + token);
 				},
 		        contentType: "application/json; charset=utf-8",
 		        dataType: "json",
 				data: JSON.stringify(newData),
-		        success: function (return_data2, status2, jqXHR2) {
-		        	loadChat();
-        		}
+		        success: loadChat
 	        });
-		}
-	});
+	   });
 }
 
 function loadChat(){
-    var owner = document.getElementById('owner').value;
-	var repo = document.getElementById('repo').value;
-    $.ajax({
-		type: "GET",
-		url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/README.md",
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		success: function (data, statuis, jqXHR) {
+   getFile(function (data, statuis, jqXHR) {
 			var chatText = document.getElementById('chatText');
 			var cont = atob(data["content"])
 			cont = cont.replace(/[\r\n]/g, "<br />");
 			chatText.innerHTML = cont;
-		}
-	});
+		});
+}
 
+function getFile(callback) {
+    var owner = document.getElementById('owner').value;
+    var repo = document.getElementById('repo').value;
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/README.md",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: callback
+    });
 }
